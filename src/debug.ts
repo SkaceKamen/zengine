9ea14e5f53379cfddcfdef9d1bc83bd95d577c3e
Jan Zípek <jan@zipek.cz>
2017-08-05 21:38:31 +0200

    Event when closing debugging window

--- src/debug.ts
+++ src/debug.ts
@@ -3,6 +3,7 @@ import { Renderer } from "./renderer";
 import { Screen } from "./screen";
 import { Scene } from "./scene"
 import { Game } from "./game";
+import { Eventor } from "./eventor";
 
 export class Debugger extends Game {
 	camera: THREE.Camera
@@ -12,27 +13,38 @@ export class Debugger extends Game {
 
 	game: Game
 
+	onClose: Eventor<void> = new Eventor<void>()
+
 	protected start() {
 		this.open();
 		super.start();
 	}
 
 	public open () {
+		var left = localStorage.getItem('left') || 0
+		var top = localStorage.getItem('top') || 0
 		var width = localStorage.getItem('width') || 800
 		var height = localStorage.getItem('height') || 640
 
-		this.window = window.open("", "_blank", "width=" + width + ",height=" + height);
+		this.window = window.open("", "_blank", "width=" + width + ",height=" + height + ",left=" + left + ",top=" + top);
 		this.window.document.head.innerHTML += "<html><head><title>ZEngine Debug</title><style>*{padding:0;margin:0;}</style></head><body></body></html>";
+		this.window.moveTo(parseFloat(left.toString()), parseFloat(top.toString()));
 
 		this.screen = new Screen(null, this.window);
 		this.renderer = new Renderer(this, this.window.document.getElementsByTagName('body')[0]);
 
 		this.loadScene(HelperScene)
 
-		window.onunload = () => {
-			localStorage.setItem('width', this.window.innerWidth.toString())
-			localStorage.setItem('height', this.window.innerHeight.toString())
+		this.window.onunload = () => {
+			localStorage.setItem('width', this.window.innerWidth.toString());
+			localStorage.setItem('height', this.window.innerHeight.toString());
+			localStorage.setItem('left', this.window.screenLeft.toString());
+			localStorage.setItem('top', this.window.screenTop.toString());
 
+			this.onClose.trigger();
+		}
+
+		window.onunload = () => {
 			this.window.close();
 		}
 	}
